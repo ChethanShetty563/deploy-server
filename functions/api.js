e3e63940d1166528cb208cf6d9d1dfd7982dc125
ChethanShetty563 <chethan563@gmail.com>
2023-06-10 20:52:59 +0530

    router

--- functions/api.js
+++ functions/api.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 
 
 
-app.get('/getUsers', (req, res) => {
+router.get('/getUsers', (req, res) => {
     UserModel.find({}, (err, result) => {
       if (err) {
         res.json(err);
@@ -28,7 +28,7 @@ app.get('/getUsers', (req, res) => {
     });
   });
   
-  app.post('/createUser', async (req, res) => {
+router.post('/createUser', async (req, res) => {
     const user = req.body;
     const newUser = new UserModel(user);
     await newUser.save();
